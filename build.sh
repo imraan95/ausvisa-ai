11e785ed20d9d8fea31c3bad372cb5033f746be8
Imraan Iqbal <m.imraan95@gmail.com>
2025-01-17 12:47:43 +1100

    Major refactor: Simplify application and use pre-trained model

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 # exit on error
 set -o errexit
 
-# Update system and install build dependencies
-apt-get update
-apt-get install -y build-essential python3-dev
-
 # Install Python dependencies
-python -m pip install --upgrade pip
-python -m pip install wheel setuptools
-python -m pip install --no-cache-dir -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Train the model
+python train_model.py
